12d20e46d8ba5acec4eb99a140cd2619e15ea9d2
Lalt <128160278+laltzn@users.noreply.github.com>
2024-06-10 08:37:56 -0300

    Update script.js

--- script.js
+++ script.js
@@ -8,18 +8,54 @@ const perguntas = [
     {
         enunciado: "Qual é uma das questões atuais enfrentadas pelas mulheres no mercado de trabalho?",
         alternativas: [
-            "a) Dificuldade de acesso à educação superior.",
-            "b) Desigualdade salarial em relação aos homens."
+            {
+                texto: "a) Dificuldade de acesso à educação superior",
+                afirmacao: "Errado!"
+            },
+            {
+                texto: "b) Desigualdade salarial em relação aos homens.",
+                afirmacao: "Correto!"
+            }
         ]
     },
 ];
 
+
 let atual = 0;
 let perguntaAtual;
+let historiaFinal = "";
 
 function mostraPergunta() {
+    if (atual >= perguntas.length) {
+        mostraResultado();
+        return;
+    }
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = "";
+    mostraAlternativas();
+}
+
+function mostraAlternativas(){
+    for(const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+}
+
+function respostaSelecionada(opcaoSelecionada) {
+    const afirmacoes = opcaoSelecionada.afirmacao;
+    historiaFinal += afirmacoes + "";
+    atual++;
+    mostraPergunta();
+}
+
+function mostraResultado() {
+    caixaPerguntas.textContent = "";
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = "";
 }
 
 mostraPergunta();
